f74be680e2a9f3afe49a50623624d6da26379657
Tiakowski <tiagosantosrazr@gmail.com>
2022-04-22 01:59:15 -0300

    Primeiro GUI

---
+++ inicio.py
@@ -0,0 +1,39 @@
+import requests
+from tkinter import *
+
+
+def pegar_cotacoes():
+    requisicao = requests.get("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL")
+
+    requisicao_dic = requisicao.json()
+
+    cotacao_dolar = requisicao_dic['USDBRL']['bid']
+    cotacao_euro = requisicao_dic['EURBRL']['bid']
+    cotacao_btc = requisicao_dic['BTCBRL']['bid']
+
+    texto = f'''
+    Dólar: {cotacao_dolar}
+    Euro: {cotacao_euro}
+    BTC: {cotacao_btc}'''
+
+    texto_cotacoes["text"] = texto
+
+
+color7 = "#E3FDFD"
+
+janela = Tk()
+janela.title("Cotação")
+janela.geometry("300x200")
+
+
+texto_inicial = Label(janela, text="Dolar/Euro/BTC:")
+texto_inicial.place(x=100,y=0)
+
+botão = Button(janela, text='Buscar Cotações', command= pegar_cotacoes)
+botão.place(x=100,y=50)
+
+texto_cotacoes = Label(janela,text="")
+texto_cotacoes.place(x=100,y=100)
+
+
+janela.mainloop()
